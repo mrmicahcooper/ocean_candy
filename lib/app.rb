d2009574b152e3b7f7b5c00e12dfb75a495ebe4e
Micah Cooper <mrmicahcoopezr@gmail.com>
2014-06-15 07:04:02 -0400

    Setup home page

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,21 @@
 require 'sinatra'
+require 'oj'
+require './lib/station'
 
 class App < Sinatra::Base
 
+
   get '/' do
     "<h1>Ocean Candy</h1>"
+  end
+
+  get '/stations' do
+    content_type :json
+    stations = Station.all.map do |station|
+      station.for_json
+    end
+
+    Oj.dump(stations, mode: :compat)
   end
 
 end
